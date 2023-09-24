2cc30f1a90d0f40fb069691766ea6e2f5598b6b0
mborgeas@gmail.com <mpo23002@byui.edu>
2023-09-24 15:30:04 +0100

    added to w01-task.html

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,33 +1,32 @@
 /* W02-Task - Profile Home Page */
-let fullname = "BORGEAS JULFÉRIQUE MPOMPA";
-console.log(fullname);
-
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
+let fullName = "BORGEAS JULFÉRIQUE MPOMPA";
+// console.log(fullName);
 const currentYear = new Date().getFullYear();
-console.log(`It is now ${currentYear}.`);
+// console.log(`It is now ${currentYear}.`);
 
-const profilePicture = "images/JulFe3+.jpg";
+const profilePicture = 'images/JulFe3+.jpg';
 
 
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById('name');
+const nameElement = document.getElementById("name");
 
-const foodElement = document.getElementById('food');
+const foodElement = document.getElementById("food");
 
-const yearElement = document.querySelector('#year');
+const yearElement = document.querySelector("#year");
 
-const imageElement = document.getElementById('home.img');
+const imageElement = document.querySelector('img[alt="Placeholder Image"]');
 
 
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = `<strong>${fullname}</strong>`;
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
-nameElement.textContent = `<strong>${currentYear}</strong>`;
-
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
 
 
 
