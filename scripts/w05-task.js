3577706f6a6d790758fbe4e471d5b34e03e628ed
mborgeas@gmail.com <mpo23002@byui.edu>
2023-10-20 14:15:22 +0100

    Added changes to w01-task.js files.

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,10 +1,17 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
+const templeList = [];
+async function geTempletData(){
+    const response = await fetch("https://byu-cse.github.io/cse121b-course/week05/temples.json")
+    templeList = await response.json()
 
+    console.log('TEMPLE DATA:', templeList);
+}
+geTempletData();
 
 /* async displayTemples Function */
-
+const displayTemples = (temples) => {};
 
 
 
